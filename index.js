a9a64dc0cec909d4bb65ac6cad623454146e4d64
Kelvys Moura <kelvysmoura2@gmail.com>
2024-07-04 09:54:36 -0300

    soma-sub

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-
-var contador = 4
-console.log(queNaoFazNada(contador))
-function queNaoFazNada(contador){
-    if(contador === 1){
-        return 1;
-    }
-    // console.log(contador) 
-    return contador * queNaoFazNada (contador-1)
+function soma(a, b){
+    return a + b;
 }
 
+function subtracao(a, b){
+    return a - b;
+}
 
+function exibirSoma(){
+    console.log('chamei o exibir soma')
+    var a = document.getElementById('valorA').value
+    console.log(parseInt(a))
 
+    var b = document.getElementById('valorB').value
+    console.log(parseInt(b))
 
+    var aNumber = parseInt(a)
+    var bNumber = parseInt(b)
 
+    console.log(soma(aNumber, bNumber))
+}
+function sub (){
+    console.log('chamei o exibir soma')
+    var a = document.getElementById('valorA').value
+    console.log(parseInt(a))
 
-// function queDiaEhHoje(){
-//     console.log(new Date())
-//     document.getElementById('data-de-hoje').innerHTML = new Date()
-// }
+    var b = document.getElementById('valorB').value
+    console.log(parseInt(b))
+
+    var aNumber = parseInt(a)
+    var bNumber = parseInt(b)
+
+    console.log(subtracao (aNumber, bNumber))
+}
